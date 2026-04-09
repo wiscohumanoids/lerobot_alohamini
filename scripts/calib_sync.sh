949cb08182af05be7284847b7cb5cefdfbe904a3
djb119 <djb119@outlook.com>
2026-04-08 22:54:30 -0500

    fix calib_sync

--- scripts/calib_sync.sh
+++ scripts/calib_sync.sh
@@ -2,22 +2,30 @@
 
 set -e
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+LEKIWI_CLIENT_DIR="/root/.cache/huggingface/lerobot/calibration/robots/lekiwi_client"
+LEKIWI_DIR="/root/.cache/huggingface/lerobot/calibration/robots/lekiwi"
+SO_DIR="/root/.cache/huggingface/lerobot/calibration/teleoperators/so_leader"
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ROBOT_DIR="$(realpath "$SCRIPT_DIR/../calibration/robot")"
+LEADER_DIR="$(realpath "$SCRIPT_DIR/../calibration/leader")"
+
 mkdir -p "$ROBOT_DIR"
+mkdir -p "$LEKIWI_CLIENT_DIR"
+
 if [ -d "$ROBOT_DIR" ]; then
-    cp -av "$ROBOT_DIR/." "/root/.cache/huggingface/lerobot/calibration/robots/lekiwi_client/"
-    cp -av "$ROBOT_DIR/." "/root/.cache/huggingface/lerobot/calibration/robots/lekiwi/"
+    cp -av "$ROBOT_DIR/." "$LEKIWI_CLIENT_DIR/"
+    cp -av "$ROBOT_DIR/." "$LEKIWI_DIR/"
 else
     echo "Error: Source directory $ROBOT_DIR does not exist."
     exit 1
 fi
 
-LEADER_DIR="$(realpath "$SCRIPT_DIR/../calibration/leader")"
+
 mkdir -p "$LEADER_DIR"
+mkdir -p "$SO_DIR"
 if [ -d "$LEADER_DIR" ]; then
-    cp -av "$LEADER_DIR/." "/root/.cache/huggingface/lerobot/calibration/teleoperators/so_leader/"
+    cp -av "$LEADER_DIR/." "$SO_DIR/"
 else
     echo "Error: Source directory $LEADER_DIR does not exist."
     exit 1
