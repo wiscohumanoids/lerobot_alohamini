9c68a2e62060b76599a634ecebbdac1f81a2f0d5
David Baghdady <stinkyshirt9@gmail.com>
2026-04-10 12:32:35 -0500

    config fixes

--- scripts/calib_sync.sh
+++ scripts/calib_sync.sh
@@ -2,31 +2,14 @@
 
 set -e
 
-LEKIWI_CLIENT_DIR="/root/.cache/huggingface/lerobot/calibration/robots/lekiwi_client"
-LEKIWI_DIR="/root/.cache/huggingface/lerobot/calibration/robots/lekiwi"
-SO_DIR="/root/.cache/huggingface/lerobot/calibration/teleoperators/so_leader"
-
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-ROBOT_DIR="$(realpath "$SCRIPT_DIR/../calibration/robot")"
-LEADER_DIR="$(realpath "$SCRIPT_DIR/../calibration/leader")"
+SRC_CONFIG_DIR="$(realpath "$SCRIPT_DIR/../calibration/")"
+HF_CONFIG_DIR="$HOME/.cache/huggingface/lerobot/"
 
-mkdir -p "$ROBOT_DIR"
-mkdir -p "$LEKIWI_CLIENT_DIR"
-
-if [ -d "$ROBOT_DIR" ]; then
-    cp -av "$ROBOT_DIR/." "$LEKIWI_CLIENT_DIR/"
-    cp -av "$ROBOT_DIR/." "$LEKIWI_DIR/"
+mkdir -p "$HF_CONFIG_DIR"
+if [ -d "$HF_CONFIG_DIR" ]; then
+    cp -r "$SRC_CONFIG_DIR" "$HF_CONFIG_DIR/"
 else
-    echo "Error: Source directory $ROBOT_DIR does not exist."
-    exit 1
-fi
-
-
-mkdir -p "$LEADER_DIR"
-mkdir -p "$SO_DIR"
-if [ -d "$LEADER_DIR" ]; then
-    cp -av "$LEADER_DIR/." "$SO_DIR/"
-else
-    echo "Error: Source directory $LEADER_DIR does not exist."
+    echo "Error copying configs!"
     exit 1
 fi
